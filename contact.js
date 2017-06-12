23c154fe5f6bc8f4590d0089419aceb12cb32655
a-sanborn <AllisonSanborn14@gmail.com>
2017-06-12 16:29:30 -0700

    Add files via upload

--- contact.js
+++ contact.js
@@ -1,12 +1,49 @@
-$('form').submit(function(event) {
-	event.preventDefault();
-	var url = $(this).attr("action");
-	var formData = $(this).serialize();
-	$.ajax(url, {
-		data: formData,
-		type: "POST",
-		success: function(response) {
-			$('#sentmessage').html("<p>Thanks for your response!</p>");
-		}
+$(function() {
+
+	// Get the form.
+	var form = $('#form');
+
+	// Get the messages div.
+	var formMessages = $('#sentmessage');
+
+	// Set up an event listener for the contact form.
+	$(form).submit(function(e) {
+		// Stop the browser from submitting the form.
+		e.preventDefault();
+
+		// Serialize the form data.
+		var formData = $(form).serialize();
+
+		// Submit the form using AJAX.
+		$.ajax({
+			type: 'POST',
+			url: $(form).attr('action'),
+			data: formData
+		})
+		.done(function(response) {
+			// Make sure that the formMessages div has the 'success' class.
+			$(formMessages).removeClass('error');
+			$(formMessages).addClass('success');
+
+			// Set the message text.
+			$(formMessages).text(response);
+
+			// Clear the form.
+			$('#name, #email, #message').val('');
+		})
+		.fail(function(data) {
+			// Make sure that the formMessages div has the 'error' class.
+			$(formMessages).removeClass('success');
+			$(formMessages).addClass('error');
+
+			// Set the message text.
+			if (data.responseText !== '') {
+				$(formMessages).text(data.responseText);
+			} else {
+				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+			}
+		});
+
 	});
+
 });
